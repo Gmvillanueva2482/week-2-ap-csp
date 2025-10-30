1debe8b5e8f34748cc8bb77aeab940b986a7a9ef
Gmvillanueva2482 <Gmvillanueva1@cps.edu>
2025-10-30 13:33:04 +0000

    online

---
+++ week11_day1.py
@@ -0,0 +1,16 @@
+print(" Welcome to the Python Review Activity!")
+print("You’ll review variables, strings, numbers, and print formatting.\n")
+
+#Step 1: create variables
+first_name="Giovanni"
+age="16"
+favorite_color="green"
+favorite_number="8"
+
+#Step 2: Pratice String Operations
+# 1. Print your name in uppercase
+print(first_name.upper())
+# 2. Print how many letters are in your name
+print(len(first_name))
+# 3. Combine your name and favorite color into one message
+print(" my name is:"+ first_name ) + (" and my fovorite color is:"+ favorite_color)
